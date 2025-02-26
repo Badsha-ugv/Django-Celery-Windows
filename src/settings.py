4c7cdd6f57896689f94d3e1d1bb9afa7e522c2a3
badsha-ugv <badsha.ugv>
2025-02-26 11:16:02 +0600

    staticfiles

--- src/settings.py
+++ src/settings.py
@@ -109,6 +109,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR /'staticfiles',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
